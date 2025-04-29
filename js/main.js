ebee24da5ec49390a3b2eb40d5d59d7ca421e1bd
rehan21054 <raihanasyam005@gmail.com>
2025-04-29 18:26:41 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('Happy Birthday').split('')
+    const titles = ('Happy Birthday Sayang').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
